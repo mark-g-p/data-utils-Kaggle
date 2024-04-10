f3292ef3ef6f64fb9b5937088ee19eeec6e6e173
mark-g-p <34350288+mark-g-p@users.noreply.github.com>
2024-04-10 12:28:35 +0200

    Add formatting using black. Add adversarial validation using CatBoostClassifier

--- EDA.py
+++ EDA.py
@@ -3,7 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import shap
 from scipy.stats import shapiro, normaltest
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from catboost import Pool, CatBoostClassifier
+
 
 class Visualiser:
     def __init__(self, dl: DataLoader, config) -> None:
@@ -12,31 +17,53 @@ class Visualiser:
         dl.train.describe()
         self.colors = config["colors"]
 
-
     def featDistribution(self) -> None:
         num_plots = len(self.cont_cols)
-        num_cols = 3  
-        num_rows = -(-num_plots // num_cols)  
-        fig, axes = plt.subplots(num_rows, num_cols, figsize=(21, 5 * num_rows))  # Adjust the figure size as needed
+        num_cols = 3
+        num_rows = -(-num_plots // num_cols)
+        fig, axes = plt.subplots(
+            num_rows, num_cols, figsize=(21, 5 * num_rows)
+        )  # Adjust the figure size as needed
 
         for i, feature in enumerate(self.cont_cols):
             row = i // num_cols
             col = i % num_cols
 
             ax = axes[row, col] if num_rows > 1 else axes[col]
-            
-            sns.histplot(self.dl.train[feature], color=self.colors[0], label='Train', alpha=0.5, bins=30, ax=ax)
-            sns.histplot(self.dl.test[feature], color=self.colors[1], label='Test', alpha=0.5, bins=30, ax=ax)
-            sns.histplot(self.dl.original[feature], color=self.colors[2], label='Original', alpha=0.5, bins=30, ax=ax)
 
-            ax.set_title(f'Distribution of {feature}')
+            sns.histplot(
+                self.dl.train[feature],
+                color=self.colors[0],
+                label="Train",
+                alpha=0.5,
+                bins=30,
+                ax=ax,
+            )
+            sns.histplot(
+                self.dl.test[feature],
+                color=self.colors[1],
+                label="Test",
+                alpha=0.5,
+                bins=30,
+                ax=ax,
+            )
+            sns.histplot(
+                self.dl.original[feature],
+                color=self.colors[2],
+                label="Original",
+                alpha=0.5,
+                bins=30,
+                ax=ax,
+            )
+
+            ax.set_title(f"Distribution of {feature}")
             ax.set_xlabel(feature)
-            ax.set_ylabel('Frequency')
+            ax.set_ylabel("Frequency")
             ax.legend()
 
         if num_plots % num_cols != 0:
             for j in range(num_plots % num_cols, num_cols):
-                axes[-1, j].axis('off')
+                axes[-1, j].axis("off")
 
         plt.tight_layout()
         plt.show()
@@ -44,53 +71,77 @@ class Visualiser:
     def featuresViolinPlot(self) -> None:
         num_plots = len(self.cont_cols)
         num_cols = 2
-        num_rows = -(-num_plots // num_cols)  
-        fig, axes = plt.subplots(num_rows, num_cols, figsize=(21, 5 * num_rows))  # Adjust the figure size as needed
+        num_rows = -(-num_plots // num_cols)
+        fig, axes = plt.subplots(
+            num_rows, num_cols, figsize=(21, 5 * num_rows)
+        )  # Adjust the figure size as needed
 
         for i, feature in enumerate(self.cont_cols):
             row = i // num_cols
             col = i % num_cols
 
             ax = axes[row, col] if num_rows > 1 else axes[col]
-    
-            sns.violinplot(x=self.dl.train[feature], color=self.colors[0], label="Train", alpha=0.5, ax=ax)
-            sns.violinplot(x=self.dl.test[feature], color=self.colors[1], label="Test", alpha=0.5, ax=ax)
-            sns.violinplot(x=self.dl.original[feature], color=self.colors[2], label="Original", alpha=0.5, ax=ax)
-            
-            ax.legend() 
-            
-            ax.set_title(f'Distribution of {feature}')
+
+            sns.violinplot(
+                x=self.dl.train[feature],
+                color=self.colors[0],
+                label="Train",
+                alpha=0.5,
+                ax=ax,
+            )
+            sns.violinplot(
+                x=self.dl.test[feature],
+                color=self.colors[1],
+                label="Test",
+                alpha=0.5,
+                ax=ax,
+            )
+            sns.violinplot(
+                x=self.dl.original[feature],
+                color=self.colors[2],
+                label="Original",
+                alpha=0.5,
+                ax=ax,
+            )
+
+            ax.legend()
+
+            ax.set_title(f"Distribution of {feature}")
             ax.set_xlabel(feature)
             ax.legend()
 
         if num_plots % num_cols != 0:
             for j in range(num_plots % num_cols, num_cols):
-                axes[-1, j].axis('off')
+                axes[-1, j].axis("off")
 
         plt.tight_layout()
         plt.show()
-        
+
+
 class StatisticalTests:
     def __init__(self, dl: DataLoader, config) -> None:
         self.dl = dl
         self.cont_cols = self.dl.test.columns
 
     def testNormality(self) -> None:
-        for (df, name) in zip([self.dl.train, self.dl.test, self.dl.original], ["Train", "Test", "Original"]):
+        for df, name in zip(
+            [self.dl.train, self.dl.test, self.dl.original],
+            ["Train", "Test", "Original"],
+        ):
             # Select only numeric columns
             print(f"\n{'-'*10}Testing features from {name}{'-'*10}\n")
-            numeric_cols = df.select_dtypes(include=['float']).columns
+            numeric_cols = df.select_dtypes(include=["float"]).columns
             normal_columns = []
 
             for col in numeric_cols:
                 # Check normality using Shapiro-Wilk test if #samples <5000, ignore null values
                 # For larger samples use D’Agostino-Pearson Test as S-W test is too sensitive
-                stat, p = 0, 0 
-                if df.shape[0] < 5000 :
+                stat, p = 0, 0
+                if df.shape[0] < 5000:
                     stat, p = shapiro(df[col].dropna())
-                    
+
                 else:
-                    stat, p = normaltest(df[col], nan_policy='omit')
+                    stat, p = normaltest(df[col], nan_policy="omit")
 
                 print(f"\nColumn: {col}")
                 print(f"Statistics={stat}, p={p}")
@@ -100,19 +151,72 @@ class StatisticalTests:
                 else:
                     print(f"{col} does not look Gaussian (reject H0)")
 
-            if len(normal_columns)>0:
+            if len(normal_columns) > 0:
                 print(normal_columns)
             else:
                 print("No features passed the Normality test")
 
+
 # fillna
 # impute
 # fill as missing
+
 
 class Imputation:
     def __init__(self) -> None:
         pass
 
 
-# Outliers
-# Binning values
+def adversarial_validation(train, test, target="target"):
+    """Do adversarial validation for chosen datasets.
+    Based on https://www.kaggle.com/code/kevinbonnes/adversarial-validation/notebook
+    """
+    # Create a 'source' feature that is 1 for 1st dataset instances and 0 for 2nd instances
+
+    train["source"] = 1
+    test["source"] = 0
+    feat_to_remove = ["source"]
+    if isinstance(target, str):
+        feat_to_remove.append(target)
+    elif isinstance(target, list):
+        feat_to_remove += target
+
+    df = pd.concat([train, test], axis=0)
+    X = df.drop(columns=feat_to_remove, errors="ignore")
+    y = df["source"]
+
+    # Identify categorical features, fortunately Catboost doesn't need to encode them
+    categorical_features_indices = np.where(X.dtypes == "object")[0]
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, test_size=0.3, random_state=0
+    )
+
+    clf = CatBoostClassifier(cat_features=categorical_features_indices, verbose=0)
+    clf.fit(X_train, y_train)
+
+    probs = clf.predict_proba(X_val)[:, 1]
+    fpr, tpr, _ = roc_curve(y_val, probs)
+    auc_score = auc(fpr, tpr)
+
+    print(f"AUC: {auc_score:.2f}")
+
+    # Plot the ROC curve
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, label=f"AUC: {auc_score:.2f}")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend(loc="lower right")
+    plt.show()
+
+    # Remove the 'source' column from the original datasets
+    train.drop("source", axis=1, inplace=True)
+    test.drop("source", axis=1, inplace=True)
+
+
+def plot_importances(model, holdout_data, features):
+    shap_values = model.get_feature_importance(holdout_data, type="ShapValues")
+    shap_values = shap_values[:, :-1]
+    shap.summary_plot(
+        shap_values, holdout_data, feature_names=features, plot_type="bar"
+    )
--- datasets.py
+++ datasets.py
@@ -3,34 +3,39 @@ import os
 import numpy as np
 from sklearn.ensemble import IsolationForest
 
-class DataLoader():
+
+class DataLoader:
     def __init__(self, config) -> None:
-        self.train             = pd.read_csv(os.path.join(config["path"],"train.csv"), index_col = 'id')
-        self.test              = pd.read_csv(os.path.join(config["path"],"test.csv"), index_col = 'id')
-        self.targets           = config["targets"]
+        self.train = pd.read_csv(
+            os.path.join(config["path"], "train.csv"), index_col="id"
+        )
+        self.test = pd.read_csv(
+            os.path.join(config["path"], "test.csv"), index_col="id"
+        )
+        self.targets = config["targets"]
         self.conjoin_orig_data = config["conjoin_orig_data"]
 
         if self.conjoin_orig_data == "Y":
-            self.original      = pd.read_csv(config["orig_path"], index_col = "id")
+            self.original = pd.read_csv(config["orig_path"], index_col="id")
         else:
-            self.original      = self.train
-        
-        self.sub_fl   = pd.read_csv(os.path.join(config["path"], "sample_submission.csv"))
+            self.original = self.train
+
+        self.sub_fl = pd.read_csv(os.path.join(config["path"], "sample_submission.csv"))
         # Standarize column names
         for tbl in [self.train, self.original, self.test]:
-            tbl.columns = tbl.columns.str.replace(r"\(|\)|\s+","", regex = True)
+            tbl.columns = tbl.columns.str.replace(r"\(|\)|\s+", "", regex=True)
             tbl.columns = map(str.lower, tbl.columns)
 
         self.delete_missing = config["delete_missing"]
-    
+
     def _addSourceCol(self) -> None:
-# Add column with source of data. Set list of feature columns names
-        self.train['Source']    = "Competition"
-        self.test['Source']     = "Competition"
-        self.original['Source'] = "Original"
+        # Add column with source of data. Set list of feature columns names
+        self.train["Source"] = "Competition"
+        self.test["Source"] = "Competition"
+        self.original["Source"] = "Original"
 
         self.feat_list = self.test.columns
-    
+
     def _displayDataHead(self, n=5) -> None:
         print(f"\nTrain set")
         print(self.train.head(n))
@@ -43,38 +48,49 @@ class DataLoader():
     def dataDescription(self) -> None:
         print(f"\n{'-'*20} Information and descriptive statistics {'-'*20}\n")
         # Creating dataset information and description:
-        for lbl, df in {'Train': self.train, 'Test': self.test, 'Original': self.original}.items():
+        for lbl, df in {
+            "Train": self.train,
+            "Test": self.test,
+            "Original": self.original,
+        }.items():
             print(f"\n{lbl} description\n")
-            print(df.describe(percentiles= [0.05, 0.25, 0.50, 0.75, 0.9, 0.95, 0.99]))
+            print(df.describe(percentiles=[0.05, 0.25, 0.50, 0.75, 0.9, 0.95, 0.99]))
             print(f"\n{lbl} information\n")
             print(df.info())
-    
+
     def _conjoinTrainOrig(self) -> None:
+        # Join two datasets and removes duplicates
         if self.conjoin_orig_data == "Y":
-            print(f"\n\nTrain shape before conjoining with original = {self.train.shape}")
-            train = pd.concat([self.train, self.original], axis=0, ignore_index = True)
+            print(
+                f"\n\nTrain shape before conjoining with original = {self.train.shape}"
+            )
+            train = pd.concat([self.train, self.original], axis=0, ignore_index=True)
             print(f"Train shape after conjoining with original= {train.shape}")
 
-            train = train.drop_duplicates(ignore_index=True)
-            print(f"Train shape after de-duping = {train.shape}")
-
             train.index = range(len(train))
-            train.index.name = 'id'
+            train.index.name = "id"
 
         else:
             print(f"\nWe are using the competition training data only")
             train = self.train
-    
+
+        train = train.drop_duplicates(ignore_index=True)
+        print(f"Train shape after de-duping = {train.shape}")
+
     def _missingData(self) -> None:
-        # Display 
-        for (df, name) in zip([self.train,self.test,self.original], ["Train", "Test", "Original"]):
+        # Display
+        for df, name in zip(
+            [self.train, self.test, self.original], ["Train", "Test", "Original"]
+        ):
             print(f"\n{'-'*20}Missing data {name}{'-'*20}\n")
             null_counts = df.isnull().sum()
             null_counts = null_counts[null_counts > 0]
             if null_counts.size > 0:
                 # print(null_counts)
                 print(f"Incomplete rows: {null_counts.size}")
-                print(f"Percentage of all rows: {(null_counts.size/df.shape[0]*100):.3f}%")
+                print(
+                    f"Percentage of all rows: {(null_counts.size/df.shape[0]*100):.3f}%"
+                )
                 if self.delete_missing == "Y":
                     print(f"\n{'-'*10}Dropping incomple rows from {name}{'-'*10}\n")
                     print(f"Dataframe size before removal {df.shape}")
@@ -82,26 +98,25 @@ class DataLoader():
                     print(f"Dataframe size after removal {df.shape}")
             else:
                 print(f"No null values found in {name} dataset.")
-                
 
-            
     def _uniqData(self) -> None:
         self._addSourceCol()
-        for (df, name) in zip([self.train,self.test,self.original], ["Train", "Test", "Original"]):
+        for df, name in zip(
+            [self.train, self.test, self.original], ["Train", "Test", "Original"]
+        ):
             print(f"\n{'-'*20}Unique value count {name} {'-'*20}\n")
             print(df[self.feat_list].nunique())
 
-        
     def _deleteMissing(self) -> None:
         # Never remove null values from test. It's expected to give results even with missing data.
         self.train = self.train.dropna()
         self.original = self.original.dropna()
-        
+
     def _removeOutliers(self) -> None:
         # Never remove outliers from test. You are expected to use whole dataset.
         # Expects that there are no missing values in the datasets.
         try:
-        # Fit the model
+            # Fit the model
             clf = IsolationForest(contamination=0.01)
             clf.fit(self.train)
 
@@ -111,5 +126,6 @@ class DataLoader():
             self.train = self.train[pred != -1]
             print(f"{anomalies.shape[0]} outliers detected and removed.")
         except ValueError:
-            print("Missing values found. Before removing outliers make sure there are no NaNs or Nulls in the dataset.")
-
+            print(
+                "Missing values found. Before removing outliers make sure there are no NaNs or Nulls in the dataset."
+            )
